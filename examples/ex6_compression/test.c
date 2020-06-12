f7c9f0d4bf90bd7b006d45fe970069890028e50d
Zhang690683220 <zhangbo_china@live.cn>
2020-06-12 00:01:30 -0400

    add zfp test

--- examples/ex6_compression/test.c
+++ examples/ex6_compression/test.c
@@ -161,14 +161,14 @@ int main(int argc, char** argv)
                 int i; // traverse all data elements
                 for(i=0;i<N*M;i++)
                 {
-                    if(max < *((double*) (data+i*sizeof(double))))
+                    if(max < *((double*) (data+i))
                     {
-                        max = *((double*) (data+i*sizeof(double)));
+                        max = *((double*) (data+i);
                     }
 
-                    if(min > *((double*) (data+i*sizeof(double))))
+                    if(min > *((double*) (data+i)
                     {
-                        min = *((double*) (data+i*sizeof(double)));
+                        min = *((double*) (data+i);
                     }
                     data_max = (double) max;
                     data_min = (double) min;
