5a3f365c891e4106ee26ffcff71708056c8e0360
Zhang690683220 <zhangbo_china@live.cn>
2020-07-16 22:38:47 -0400

    subset test

--- examples/ex6_compression/get_server.c
+++ examples/ex6_compression/get_server.c
@@ -33,8 +33,8 @@ int main(int argc, char** argv)
 
         uint64_t lb[2] = {0}, ub[2] = {0};
 
-        ub[0] = 7; 
-        ub[1] = 7;
+        ub[0] = N-1; 
+        ub[1] = M-1;
 
 
         dspaces_get(var_name, timestep, sizeof(double), ndim, lb, ub, data);
